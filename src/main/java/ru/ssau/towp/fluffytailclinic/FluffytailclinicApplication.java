9994bb97a742f8f4f860ab10825c26a053c0ef20
aniam <aniamurzina@gmail.com>
2025-02-16 23:03:28 +0400

    Draft Spring Boot Starter

---
+++ src/main/java/ru/ssau/towp/fluffytailclinic/FluffytailclinicApplication.java
@@ -0,0 +1,13 @@
+package ru.ssau.towp.fluffytailclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffytailclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffytailclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ssau/towp/fluffytailclinic/FluffytailclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssau.towp.fluffytailclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffytailclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
